b3eb06b5900f3f8a873b1c9e12e4125aac8ce1a3
Michael Bridgen <michael@weave.works>
2020-11-10 20:55:58 +0000

    Add authenticated registry test handler
    This is to make it possible to test that the controller will pick up
    and use a credentials secret.
    
    Signed-off-by: Michael Bridgen <michael@weave.works>

--- controllers/registry_test.go
+++ controllers/registry_test.go
@@ -17,11 +17,13 @@ limitations under the License.
 package controllers
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 
+	"github.com/google/go-containerregistry/pkg/authn"
 	"github.com/google/go-containerregistry/pkg/name"
 	"github.com/google/go-containerregistry/pkg/registry"
 	"github.com/google/go-containerregistry/pkg/v1/random"
@@ -48,13 +50,36 @@ var _ = Context("Registry handler", func() {
 
 // ---
 
+// pre-populated db of tags, so it's not necessary to upload images to
+// get results from remote.List.
+var convenientTags = map[string][]string{
+	"convenient": []string{
+		"tag1", "tag2",
+	},
+}
+
 // set up a local registry for testing scanning
 func newRegistryServer() *httptest.Server {
 	regHandler := registry.New()
 	srv := httptest.NewServer(&tagListHandler{
 		registryHandler: regHandler,
-		imagetags:       map[string][]string{},
+		imagetags:       convenientTags,
 	})
+	return srv
+}
+
+func newAuthenticatedRegistryServer(username, pass string) *httptest.Server {
+	regHandler := registry.New()
+	regHandler = &tagListHandler{
+		registryHandler: regHandler,
+		imagetags:       convenientTags,
+	}
+	regHandler = &authHandler{
+		registryHandler: regHandler,
+		allowedUser:     username,
+		allowedPass:     pass,
+	}
+	srv := httptest.NewServer(regHandler)
 	return srv
 }
 
@@ -119,3 +144,83 @@ func (h *tagListHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
+
+// there's no authentication in go-containerregistry/pkg/registry;
+// this wrapper adds basic auth to a registry handler. NB: the
+// important thing is to be able to test that the credentials get from
+// the secret to the registry API library; it's assumed that the
+// registry API library does e.g., OAuth2 correctly.  See
+// https://tools.ietf.org/html/rfc7617 regarding basic authentication.
+
+type authHandler struct {
+	allowedUser, allowedPass string
+	registryHandler          http.Handler
+}
+
+// ServeHTTP serves a request which needs authentication.
+func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		w.Header().Add("WWW-Authenticate", `Basic realm="Registry"`)
+		w.WriteHeader(401)
+		return
+	}
+	if !strings.HasPrefix(authHeader, "Basic ") {
+		w.WriteHeader(403)
+		w.Write([]byte(`Authorization header does not being with "Basic "`))
+		return
+	}
+	namePass, err := base64.StdEncoding.DecodeString(authHeader[6:])
+	if err != nil {
+		w.WriteHeader(403)
+		w.Write([]byte(`Authorization header doesn't appear to be base64-encoded`))
+		return
+	}
+	namePassSlice := strings.SplitN(string(namePass), ":", 2)
+	if len(namePassSlice) != 2 {
+		w.WriteHeader(403)
+		w.Write([]byte(`Authorization header doesn't appear to be colon-separated value `))
+		w.Write(namePass)
+		return
+	}
+	if namePassSlice[0] != h.allowedUser || namePassSlice[1] != h.allowedPass {
+		w.WriteHeader(403)
+		w.Write([]byte(`Authorization failed: wrong username or password`))
+		return
+	}
+	h.registryHandler.ServeHTTP(w, r)
+}
+
+var _ = Context("Authentication handler", func() {
+
+	var registryServer *httptest.Server
+	var username, password string
+
+	BeforeEach(func() {
+		username = "user"
+		password = "password1"
+		registryServer = newAuthenticatedRegistryServer(username, password)
+	})
+
+	AfterEach(func() {
+		registryServer.Close()
+	})
+
+	It("rejects requests without authentication", func() {
+		repo, err := name.NewRepository(strings.TrimPrefix(registryServer.URL, "http://") + "/convenient")
+		Expect(err).ToNot(HaveOccurred())
+		_, err = remote.List(repo)
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("accepts requests with correct authentication", func() {
+		repo, err := name.NewRepository(strings.TrimPrefix(registryServer.URL, "http://") + "/convenient")
+		Expect(err).ToNot(HaveOccurred())
+		auth := &authn.Basic{
+			Username: username,
+			Password: password,
+		}
+		_, err = remote.List(repo, remote.WithAuth(auth))
+		Expect(err).ToNot(HaveOccurred())
+	})
+})
