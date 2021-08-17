650775e07995e451746f18494d1b86f70cbacdd6
Michael Bridgen <michael@weave.works>
2021-08-17 13:52:59 +0100

    Exit early when resource is suspended
    The contract for .spec.suspend is (I'm told) to not take any actions,
    including not updating the status.
    
    Signed-off-by: Michael Bridgen <michael@weave.works>

--- controllers/imagerepository_controller.go
+++ controllers/imagerepository_controller.go
@@ -109,6 +109,9 @@ func (r *ImageRepositoryReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 	}
 
 	r.RecordSuspend(ctx, &imageRepo, imageRepo.Spec.Suspend)
+	if imageRepo.Spec.Suspend {
+		return ctrl.Result{}, nil
+	}
 
 	patcher, err := patch.NewHelper(&imageRepo, r.Client)
 	if err != nil {
@@ -132,17 +135,6 @@ func (r *ImageRepositoryReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 		r.RecordReadiness(ctx, &imageRepo)
 		r.RecordDuration(ctx, &imageRepo, reconcileStart)
 	}()
-
-	if imageRepo.Spec.Suspend {
-		msg := "ImageRepository is suspended, skipping reconciliation"
-		conditions.MarkFalse(
-			&imageRepo,
-			meta.ReadyCondition,
-			meta.SuspendedReason,
-			msg,
-		)
-		return ctrl.Result{}, nil
-	}
 
 	log := logr.FromContext(ctx)
 
