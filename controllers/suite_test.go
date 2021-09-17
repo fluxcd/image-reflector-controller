bdb6dbff8b505fd5744231774013bc7ea5204452
Sunny <darkowlzz@protonmail.com>
2021-09-17 19:08:37 +0530

    Add testenv TestMain and move ginkgo to legacy
    Add new testenv based TestMain in suite_test.go and move the ginkgo test
    setup to legacy_suite_test.go. This helps to run both the ginkgo tests
    and testenv based tests.
    
    Update fluxcd/pkg/runtime to update testenv with the fix for glog and
    klog to coexist. Badger uses glog and its flags conflicts with the klog
    flags added by testenv.
    
    Signed-off-by: Sunny <darkowlzz@protonmail.com>

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -17,7 +17,7 @@ limitations under the License.
 package controllers
 
 import (
-	"context"
+	"fmt"
 	"math/rand"
 	"os"
 	"path/filepath"
@@ -25,20 +25,22 @@ import (
 	"time"
 
 	"github.com/dgraph-io/badger/v3"
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	"github.com/fluxcd/pkg/runtime/testenv"
 
 	imagev1 "github.com/fluxcd/image-reflector-controller/api/v1beta1"
 	"github.com/fluxcd/image-reflector-controller/internal/database"
 	// +kubebuilder:scaffold:imports
 )
+
+// These tests make use of plain Go using Gomega for assertions.
+// At the beginning of every (sub)test Gomega can be initialized
+// using gomega.NewWithT.
+// Refer to http://onsi.github.io/gomega/ to learn more about
+// Gomega.
 
 // for Eventually
 const (
@@ -48,94 +50,73 @@ const (
 	reconciliationInterval = time.Second * 2
 )
 
-// These tests use Ginkgo (BDD-style Go testing framework). Refer to
-// http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
+var (
+	testEnv      *testenv.Environment
+	testBadgerDB *badger.DB
+	ctx          = ctrl.SetupSignalHandler()
+)
 
-var cfg *rest.Config
-var k8sClient client.Client
-var k8sMgr ctrl.Manager
-var stopManager func()
-var imageRepoReconciler *ImageRepositoryReconciler
-var imagePolicyReconciler *ImagePolicyReconciler
-var testEnv *envtest.Environment
-var badgerDir string
-var badgerDB *badger.DB
-
-func TestAPIs(t *testing.T) {
-	RegisterFailHandler(Fail)
-
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+func init() {
+	rand.Seed(time.Now().UnixNano())
 }
 
-var _ = BeforeSuite(func(done Done) {
-	ctrl.SetLogger(
-		zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)),
-	)
+func TestMain(m *testing.M) {
 
-	By("bootstrapping test environment")
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
-	}
+	utilruntime.Must(imagev1.AddToScheme(scheme.Scheme))
+
+	testEnv = testenv.New(testenv.WithCRDPath(filepath.Join("..", "config", "crd", "bases")))
 
 	var err error
-	cfg, err = testEnv.Start()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(cfg).ToNot(BeNil())
-
-	err = imagev1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	// +kubebuilder:scaffold:scheme
-
-	k8sMgr, err = ctrl.NewManager(cfg, ctrl.Options{
-		Scheme: scheme.Scheme,
-	})
-	Expect(err).ToNot(HaveOccurred())
-
-	badgerDir, err = os.MkdirTemp(os.TempDir(), "badger")
-	Expect(err).ToNot(HaveOccurred())
-	badgerDB, err = badger.Open(badger.DefaultOptions(badgerDir))
-	Expect(err).ToNot(HaveOccurred())
-
-	imageRepoReconciler = &ImageRepositoryReconciler{
-		Client:   k8sMgr.GetClient(),
-		Scheme:   scheme.Scheme,
-		Database: database.NewBadgerDatabase(badgerDB),
+	var badgerDir string
+	badgerDir, err = os.MkdirTemp("", "badger")
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create temporary directory for badger: %v", err))
 	}
-	Expect(imageRepoReconciler.SetupWithManager(k8sMgr, ImageRepositoryReconcilerOptions{})).To(Succeed())
-
-	imagePolicyReconciler = &ImagePolicyReconciler{
-		Client:   k8sMgr.GetClient(),
-		Scheme:   scheme.Scheme,
-		Database: database.NewBadgerDatabase(badgerDB),
+	testBadgerDB, err = badger.Open(badger.DefaultOptions(badgerDir))
+	if err != nil {
+		panic(fmt.Sprintf("Failed to create new Badger database: %v", err))
 	}
-	Expect(imagePolicyReconciler.SetupWithManager(k8sMgr, ImagePolicyReconcilerOptions{})).To(Succeed())
 
-	// this must be started for the caches to be running, and thereby
-	// for the client to be usable.
-	mgrContext, cancel := context.WithCancel(ctrl.SetupSignalHandler())
+	if err = (&ImageRepositoryReconciler{
+		Client:   testEnv,
+		Scheme:   scheme.Scheme,
+		Database: database.NewBadgerDatabase(testBadgerDB),
+	}).SetupWithManager(testEnv, ImageRepositoryReconcilerOptions{}); err != nil {
+		panic(fmt.Sprintf("Failed to start ImageRepositoryReconciler: %v", err))
+	}
+
+	if err = (&ImagePolicyReconciler{
+		Client:   testEnv,
+		Scheme:   scheme.Scheme,
+		Database: database.NewBadgerDatabase(testBadgerDB),
+	}).SetupWithManager(testEnv, ImagePolicyReconcilerOptions{}); err != nil {
+		panic(fmt.Sprintf("Failed to start ImagePolicyReconciler: %v", err))
+	}
+
 	go func() {
-		err = k8sMgr.Start(mgrContext)
-		Expect(err).ToNot(HaveOccurred())
+		fmt.Println("Starting the test environment")
+		if err := testEnv.Start(ctx); err != nil {
+			panic(fmt.Sprintf("Failed to start the test environment manager: %v", err))
+		}
 	}()
-	stopManager = cancel
+	<-testEnv.Manager.Elected()
 
-	k8sClient = k8sMgr.GetClient()
-	Expect(k8sClient).ToNot(BeNil())
+	code := m.Run()
 
-	close(done)
-}, 60)
+	fmt.Println("Stopping the test environment")
+	if err := testEnv.Stop(); err != nil {
+		panic(fmt.Sprintf("Failed to stop the test environment: %v", err))
+	}
 
-var _ = AfterSuite(func() {
-	By("tearing down the test environment")
-	stopManager()
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(badgerDB.Close()).ToNot(HaveOccurred())
-	Expect(os.RemoveAll(badgerDir)).ToNot(HaveOccurred())
-})
+	if err := testBadgerDB.Close(); err != nil {
+		panic(fmt.Sprintf("Failed to close Badger: %v", err))
+	}
+	if err := os.RemoveAll(badgerDir); err != nil {
+		panic(fmt.Sprintf("Failed to remove Badger dir: %v", err))
+	}
+
+	os.Exit(code)
+}
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz1234567890")
 
